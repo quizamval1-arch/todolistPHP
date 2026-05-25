b9efcfdba28080da6fca06a50eff9846ce00a228
Valerie <quizamval1@gmail.com>
2026-05-25 18:01:11 -0500

    fin updatetask

--- updateTask.php
+++ updateTask.php
@@ -4,6 +4,14 @@ $id = $_POST;
 $nombre =$_POST;
 $detalles =$_POST;
 
+$sql="UPDATE tareas SET nombre='$nombre', detalles='$detalles' WHERE id=$id";
 
+if($conn->query($sql) == true){
+    echo"<script> alert (Se ha podido actualizar el dato)</script>";
+    header('location:dashboardList.php');
+}else{
+    echo"<script> alert (No se ha podido actualizar el dato)</script>";
+    header('location:dashboardList.php');
+}
 
 ?>
